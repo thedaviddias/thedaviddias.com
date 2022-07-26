39460272b8f0ba5b8229eb3839804bb4dcf598f4
David Dias <thedaviddias@gmail.com>
2022-07-26 00:25:53 -0400

    try again

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@ const nextTranslate = require('next-translate')
 
 module.exports = nextTranslate({
   reactStrictMode: true,
-  eslint: {
-    dirs: ['src'],
-  },
+  // eslint: {
+  //   dirs: ['src'],
+  // },
   images: {
     domains: ['img.youtube.com', 'media.giphy.com'],
   },
+  // ignoreDuringBuilds: true,
   async rewrites() {
     return [
       {

5e7e5464acc8be62ad3ce625a3a50c7d57c18de6
David Dias <thedaviddias@gmail.com>
2022-07-26 22:35:57 -0400

    try more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
   plugins: ['prettier'],
 }

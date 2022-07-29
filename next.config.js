dce82d64a9b473131bca3911acfbe27b8585dbbe
David Dias <thedaviddias@gmail.com>
2022-07-29 01:06:53 -0400

    remove header cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
-// const path = require('path')
 
 const nextConfig = nextTranslate({
   reactStrictMode: true,

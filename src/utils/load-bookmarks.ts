02338fdeaf5a31c1c6f964bede245506b113a477
David Dias <thedaviddias@gmail.com>
2022-08-01 17:34:33 -0400

    add code bookmarks

--- src/utils/load-bookmarks.ts
+++ src/utils/load-bookmarks.ts
@@ -1,14 +1,47 @@
-export async function loadBookmarks() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(
-    `https://api.raindrop.io/rest/v1/raindrops/${process.env.RAINDROP_COLLECTION}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`,
-      },
-    }
-  )
-  const data = await res.json()
+type Bookmarks = {
+  excerpt: string
+  note: string
+  type: string
+  cover: string
+  tags: string[]
+  removed: false
+  _id: number
+  collection: unknown[]
+  user: unknown[]
+  link: string
+  title: string
+  domain: string
+  lastUpdate: string
+  sort: number
+  media: unknown[]
+  created: string
+  cache: unknown[]
+  highlights: []
+  collectionId: number
+}
 
-  return data
+type LoadBookmarksResponse = {
+  result: boolean
+  items: Bookmarks[]
+  count: number
+  collectionId: number
+}
+
+export async function loadBookmarks(): Promise<LoadBookmarksResponse | undefined> {
+  try {
+    const res = await fetch(
+      `https://api.raindrop.io/rest/v1/raindrops/${process.env.RAINDROP_COLLECTION}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`,
+        },
+      }
+    )
+    const data = await res.json()
+
+    return data
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('error', error)
+  }
 }

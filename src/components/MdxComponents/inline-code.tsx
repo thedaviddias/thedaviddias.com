fd458f2d6461f8b6fc323ed4b2fb1fa594db8638
David Dias <thedaviddias@gmail.com>
2022-08-14 11:16:09 -0400

    fix inline code

--- src/components/MdxComponents/inline-code.tsx
+++ src/components/MdxComponents/inline-code.tsx
@@ -3,5 +3,9 @@ type InlineCodeProps = {
 }
 
 export const InlineCode: React.FC<InlineCodeProps> = ({ children }) => {
-  return <code className="dark:text-white dark:bg-slate-700">{children}</code>
+  return (
+    <code className="before:!content-[''] after:!content-[''] dark:text-white bg-slate-100 dark:bg-slate-700 rounded-sm border-[1px] border-gray-400 dark:border-gray-400 px-1 py-0.5">
+      {children}
+    </code>
+  )
 }

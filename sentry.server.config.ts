94ace1fff21a7aa39f6aef712f34ba971e70c22e
David Dias <thedaviddias@gmail.com>
2023-01-04 19:07:05 -0500

    fix: set sentry transactions to 0

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,6 +8,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://0823630951cd4283831b65772dc9ed58@o515454.ingest.sentry.io/6601262',
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0,
   denyUrls: ['localhost'],
 })
